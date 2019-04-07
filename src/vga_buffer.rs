7402e9f1b46fb11af075735e88693f1dce36e819
j3elam <j3elam@gmail.com>
2019-04-07 00:26:43 -0500

    Add structs to store raw data for VGA

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -19,3 +19,35 @@ pub enum Color {
     Yellow = 14,
     White = 15,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode{
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize, 
+    color_code: ColorCode, 
+    buffer: &'static mut Buffer,
+}
+
